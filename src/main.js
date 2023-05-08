e66ee54d46db2268d855d1d957697a47f1826d2c
Matheus Gomes <matheusoutros@hotmail.com>
2023-05-08 15:22:41 -0300

    Segundo desafio solucionado

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@
 // alert(pergunta*2)
 
 
+//Segundo desafio, calcule o valor do jantar!
+// const valorDoJantar = parseInt(prompt("Me informe o valor do jantar"));
+// const taxaGarcon = 12;
+// alert("A taxa do garçon é R$12,00.");
+// const resultadoFinal = valorDoJantar + taxaGarcon;
+// alert(`"O valor total do Jantar sairá por ${resultadoFinal}"`)
 
