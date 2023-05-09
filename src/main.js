bed6ad22119252527298b2037d18878693acf1a4
Matheus Gomes <matheusoutros@hotmail.com>
2023-05-08 23:04:49 -0300

    Adicionando o 2° desafio realizado

--- src/main.js
+++ src/main.js
@@ -2,19 +2,26 @@
 const clickMultiplier = document.querySelector('#clickMultiplier');
 let resultado = document.querySelector('#receiveNumber');
 
-function multiplier(evento) {
-    evento.preventDefault();
+function multiplier() {
     const pergunta = parseInt(prompt("Me fale um número que eu devolverei o dobro"));
-    return resultado.innerHTML = "O resultado é " + pergunta*2
+    return resultado.innerHTML = "O resultado é " + (pergunta*2).toFixed(2);
 }
 clickMultiplier.onclick = multiplier;
 
-//Segundo desafio, calcule o valor do jantar!
-// const valorDoJantar = parseInt(prompt("Me informe o valor do jantar"));
-// const taxaGarcon = 12;
-// alert("A taxa do garçon é R$12,00.");
-// const resultadoFinal = valorDoJantar + taxaGarcon;
-// alert(`"O valor total do Jantar sairá por ${resultadoFinal}"`)
+
+
+const clickDinner = document.querySelector('#linkDinner');
+let resultDinner = document.querySelector('#resultDinner');
+
+function calculateDinner() {
+    const valueDinner = parseInt(prompt("Me informe o valor do jantar"));
+    alert("A taxa do garçon é R$12,00.");
+    const rateWaiter = 12;
+    const resultFinal = valueDinner + rateWaiter
+    return resultDinner.innerHTML = "Valor do jantar ficou por R$" + resultFinal.toFixed(2);
+}
+clickDinner.onclick = calculateDinner;
+
 
 
 //Terceiro desafio, programa para uma pizzaria!
